ff7045480de4a0aaf1ca36affa1115225e8758ac
JaredPBaker <67445065+JaredPBaker@users.noreply.github.com>
2024-04-09 19:48:53 +0000

    increased functionality and improved aesthetics

--- scripts.js
+++ scripts.js
@@ -1,24 +1,33 @@
 function openModal(projectId, projectDescription) {
-    var modal = document.getElementById("modal" + projectId.replace(/ /g, ''));
-    modal.style.display = "block";
-  }
-  
-  function closeModal(modalId) {
-    var modal = document.getElementById(modalId);
-    modal.style.display = "none";
-  }
-  
-  window.onclick = function(event) {
-    var modals = document.getElementsByClassName('modal');
-    for (var i = 0; i < modals.length; i++) {
-      var modal = modals[i];
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
+  var modal = document.getElementById("modal" + projectId.replace(/ /g, ''));
+  modal.style.display = "block";
+}
+
+function closeModal(modalId) {
+  var modal = document.getElementById(modalId);
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  var modals = document.getElementsByClassName('modal');
+  for (var i = 0; i < modals.length; i++) {
+    var modal = modals[i];
+    if (event.target == modal) {
+      modal.style.display = "none";
     }
   }
-  
-  // Update current year in the footer
-  var currentYear = new Date().getFullYear();
-  document.getElementById("currentYear").innerText = currentYear;
-  
+}
+
+// Update current year in the footer
+var currentYear = new Date().getFullYear();
+document.getElementById("currentYear").innerText = currentYear;
+
+function scrollToSection(id) {
+  const section = document.getElementById(id);
+  if (section) {
+    window.scrollTo({
+      behavior: 'smooth',
+      top: section.offsetTop,
+    });
+  }
+}
